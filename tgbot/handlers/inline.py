afa684955750c337d84b02780cd7b25dad65d5b8
Roman Chursanov <authfailed@gmail.com>
2025-01-12 19:17:29 +0500

    Добавлен локальный список провайдеров

--- tgbot/handlers/inline.py
+++ tgbot/handlers/inline.py
@@ -1,40 +1,183 @@
 from aiogram import Router, F, Bot, types
 from aiogram.types import InlineQuery, InlineQueryResultArticle, InputTextMessageContent
-import requests
-import json
-from functools import lru_cache
 
 inline_router = Router()
 
-
-@lru_cache(maxsize=1)
-def fetch_mna_data():
-    response = requests.get('https://helper.chrsnv.ru/api/mna.json', verify=False)
-    return response.json()['mna']
-
+# Локальный список MnA
+MNA_DATA = [
+    {
+        "name": "Interzet",
+        "authorization": "IPoE Static",
+        "connection": "Ethernet",
+        "link": "https://clever.ertelecom.ru/content/space/4/folder/582/article/3289"
+    },
+    {
+        "name": "Тура-Telecom",
+        "authorization": "DHCP / PPPoE",
+        "connection": "Ethernet",
+        "link": "https://clever.ertelecom.ru/content/space/4/folder/582/article/3280"
+    },
+    {
+        "name": "Ярославль Телесеть (ЯТС)",
+        "authorization": "DHCP / PPPoE",
+        "connection": "Ethernet",
+        "link": "https://clever.ertelecom.ru/content/space/4/folder/582/article/3136"
+    },
+    {
+        "name": "Иркнет",
+        "authorization": "PPPoE",
+        "connection": "ADSL / GPON / FTTH",
+        "link": "https://clever.ertelecom.ru/content/space/4/folder/582/article/3192"
+    },
+    {
+        "name": "Контакт (Тула)",
+        "authorization": "PPPoE",
+        "connection": "ADSL",
+        "link": "https://clever.ertelecom.ru/content/space/4/folder/582/article/3282"
+    },
+    {
+        "name": "Коламбия Телеком",
+        "authorization": "DHCP / PPPoE",
+        "connection": "Ethernet / ADSL",
+        "link": "https://clever.ertelecom.ru/content/space/4/folder/582/article/2761"
+    },
+    {
+        "name": "Телемир (Липецк)",
+        "authorization": "DHCP / PPPoE",
+        "connection": "Ethernet",
+        "link": "https://clever.ertelecom.ru/content/space/4/article/3191"
+    },
+    {
+        "name": "Акадо (СПБ)",
+        "authorization": "DHCP mac+vlan",
+        "connection": "Ethernet",
+        "link": "https://clever.ertelecom.ru/content/space/4/folder/582/article/3291"
+    },
+    {
+        "name": "БКС (Брянск)",
+        "authorization": "DHCP mac+vlan / DHCP",
+        "connection": "Ethernet / PON",
+        "link": "https://clever.ertelecom.ru/content/space/4/folder/582/article/2760"
+    },
+    {
+        "name": "Инфоцентр (Сосновый бор)",
+        "authorization": "IPoE Static",
+        "connection": "Ethernet",
+        "link": "https://clever.ertelecom.ru/content/space/4/folder/582/article/3285"
+    },
+    {
+        "name": "Конвекс/ТКС (ЕКБ)",
+        "authorization": "IPoE Static / PPTP (общежития)",
+        "connection": "Ethernet",
+        "link": "https://clever.ertelecom.ru/content/space/4/folder/582/article/2769"
+    },
+    {
+        "name": "Дельта-Телеком",
+        "authorization": "PPPoE",
+        "connection": "ADSL",
+        "link": "https://clever.ertelecom.ru/content/space/4/folder/582/article/3106"
+    },
+    {
+        "name": "Мегаполис-Телеком",
+        "authorization": "PPPoE",
+        "connection": "PON",
+        "link": "https://clever.ertelecom.ru/content/space/4/article/3189"
+    },
+    {
+        "name": "СибТелеКом (СТК)",
+        "authorization": "DHCP / PPPoE",
+        "connection": "Ethernet / ADSL",
+        "link": "https://clever.ertelecom.ru/content/space/4/folder/582/article/3121"
+    },
+    {
+        "name": "Westcall / N-Link (Рязань)",
+        "authorization": "DHCP / PPPoE",
+        "connection": "Ethernet / ADSL / PON",
+        "link": "https://clever.ertelecom.ru/content/space/4/folder/582/article/3288"
+    },
+    {
+        "name": "Акадо (ЕКБ)",
+        "authorization": "L2TP",
+        "connection": "Ethernet",
+        "link": "https://clever.ertelecom.ru/content/space/4/folder/582/article/2768"
+    },
+    {
+        "name": "Сатурн (Пермь)",
+        "authorization": "PPPoE",
+        "connection": "FTTH",
+        "link": "https://clever.ertelecom.ru/content/space/4/folder/582/article/6528"
+    },
+    {
+        "name": "Westcall (СПб)",
+        "authorization": "DHCP mac+vlan",
+        "connection": "Ethernet",
+        "link": "https://clever.ertelecom.ru/content/space/4/folder/582/article/3286"
+    },
+    {
+        "name": "Кредолинк (СПб)",
+        "authorization": "DHCP mac+vlan",
+        "connection": "Ethernet",
+        "link": "https://clever.ertelecom.ru/content/space/4/folder/582/article/3283"
+    },
+    {
+        "name": "MSNet (СПБ)",
+        "authorization": "DHCP mac+vlan",
+        "connection": "Ethernet",
+        "link": "https://clever.ertelecom.ru/content/space/4/folder/582/article/3287"
+    },
+    {
+        "name": "Стрела (Димитровград)",
+        "authorization": "DHCP",
+        "connection": "Ethernet",
+        "link": "https://clever.ertelecom.ru/content/space/4/folder/582/article/3172"
+    },
+    {
+        "name": "С-Телеком МСК (Самолет)",
+        "authorization": "DHCP / PPPoE",
+        "connection": "Ethernet",
+        "link": "https://clever.ertelecom.ru/content/space/4/folder/582/article/3238"
+    },
+    {
+        "name": "Дианэт (Барнаул и область)",
+        "authorization": "PPPoE / L2TP",
+        "connection": "Ethernet",
+        "link": "https://clever.ertelecom.ru/content/space/4/folder/582/article/2646"
+    },
+    {
+        "name": "НТС (Томск)",
+        "authorization": "IPoE DHCP",
+        "connection": "Ethernet",
+        "link": "https://clever.ertelecom.ru/content/space/4/article/3648"
+    },
+    {
+        "name": "Кроникс/Rinet",
+        "authorization": "DHCP / DHCP mac+vlan",
+        "connection": "Ethernet",
+        "link": "https://clever.ertelecom.ru/content/space/4/article/3246/page/3"
+    }
+]
 
 @inline_router.inline_query()
 async def handle_inline_query(query: InlineQuery):
     try:
-        mna_data = fetch_mna_data()  # This is now a synchronous call
         results = []
 
         # Filter data if query is provided
         search_query = query.query.lower()
         filtered_data = [
-            item for item in mna_data
+            item for item in MNA_DATA
             if search_query in item['name'].lower() or
                search_query in item['authorization'].lower() or
                search_query in item['connection'].lower()
-        ] if search_query else mna_data
+        ] if search_query else MNA_DATA
 
         # Create results
         for idx, item in enumerate(filtered_data):
             message_text = (
                 f"📡 *{item['name']}*\n\n"
                 f"🔐 Авторизация: {item['authorization']}\n"
-                f"🔌 Подключение: {item['connection']}\n"
-                f"🔗 [Подробнее]({item['link']})"
+                f"🔌 Подключение: {item['connection']}\n\n"
+                f"[Провайдер в БЗ]({item['link']})"
             )
 
             results.append(
